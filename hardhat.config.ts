2d0ad0d92f603a7c14d73b4cbb6b541826ead7aa
kei-matsuoka <keimatsuoka0311@gmail.com>
2023-11-15 14:02:41 +0900

    Hardhatの設定修正

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: '0.8.20',
+  networks: {
+    mumbai: {
+      url: 'https://polygon-mumbai-pokt.nodies.app',
+      accounts: ['0x' + process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
 };
 
 export default config;
