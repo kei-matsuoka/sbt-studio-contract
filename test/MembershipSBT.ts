167bb0f5bec3e279af37fb575ac146ae8dabf60c
kei-matsuoka <keimatsuoka0311@gmail.com>
2024-01-05 18:11:34 +0900

    test追加

--- test/MembershipSBT.ts
+++ test/MembershipSBT.ts
@@ -8,33 +8,32 @@ const metadata = {
   description:
     'This is a membership token for the Fitness Gym. It is a soul-bound token (SBT) that can be used to redeem a 1 year membership at the Fitness Gym.',
 };
-const BurnAuth = { IssuerOnly: 0, OwnerOnly: 1, Both: 2, Neither: 3 };
+
+const name = 'Fitness Gym Membership';
+const symbol = 'FGM';
+const baseURI =
+  'https://ipfs.io/ipfs/QmVLqt2tN4NCeYdCsTg1rMeZTmT2D8spowhSnNSLuXtsM2/';
+const maxSupply = 2;
+const burnAuth = { IssuerOnly: 0, OwnerOnly: 1, Both: 2, Neither: 3 };
 
 describe('MembershipSBT', function () {
   async function deployMembershipSBTFixture() {
     const [owner, addr1, addr2] = await ethers.getSigners();
 
     const membershipSBT = await ethers.deployContract('MembershipSBT', [
-      'Fitness Gym Membership',
-      'FGM',
-      'https://ipfs.io/ipfs/QmVLqt2tN4NCeYdCsTg1rMeZTmT2D8spowhSnNSLuXtsM2/',
-      2,
-      BurnAuth.IssuerOnly,
+      name,
+      symbol,
+      baseURI,
+      maxSupply,
+      burnAuth.IssuerOnly,
       owner.address,
     ]);
     await membershipSBT.waitForDeployment();
 
-    return { membershipSBT, owner, addr1, addr2, BurnAuth };
+    return { membershipSBT, owner, addr1, addr2, burnAuth };
   }
 
   describe('Deployment', function () {
-    it('Should set the right owner', async function () {
-      const { membershipSBT, owner } = await loadFixture(
-        deployMembershipSBTFixture
-      );
-      expect(await membershipSBT.owner()).to.equal(owner.address);
-    });
-
     it('Should set the right name', async function () {
       const { membershipSBT } = await loadFixture(deployMembershipSBTFixture);
       expect(await membershipSBT.name()).to.equal('Fitness Gym Membership');
@@ -48,6 +47,76 @@ describe('MembershipSBT', function () {
     it('Should set the right maxSupply', async function () {
       const { membershipSBT } = await loadFixture(deployMembershipSBTFixture);
       expect(await membershipSBT.maxSupply()).to.equal(2);
+    });
+
+    it('Should set the right burnAuth', async function () {
+      const { membershipSBT } = await loadFixture(deployMembershipSBTFixture);
+      expect(await membershipSBT.burnAuth(0)).to.equal(0);
+    });
+
+    it('Should set the right owner', async function () {
+      const { membershipSBT, owner } = await loadFixture(
+        deployMembershipSBTFixture
+      );
+      expect(await membershipSBT.owner()).to.equal(owner.address);
+    });
+  });
+
+  describe('Locking', function () {
+    it('Should return true if locked', async function () {
+      const { membershipSBT } = await loadFixture(deployMembershipSBTFixture);
+      await membershipSBT.lock(0);
+      expect(await membershipSBT.locked(0)).to.equal(true);
+    });
+
+    it('Should return false if not locked', async function () {
+      const { membershipSBT } = await loadFixture(deployMembershipSBTFixture);
+      expect(await membershipSBT.locked(0)).to.equal(false);
+    });
+
+    it('Should lock the token', async function () {
+      const { membershipSBT } = await loadFixture(deployMembershipSBTFixture);
+      await membershipSBT.lock(0);
+      expect(await membershipSBT.locked(0)).to.equal(true);
+    });
+
+    it('Should emit a Locked event', async function () {
+      const { membershipSBT } = await loadFixture(deployMembershipSBTFixture);
+      await expect(membershipSBT.lock(0))
+        .to.emit(membershipSBT, 'Locked')
+        .withArgs(0);
+    });
+
+    it('Should unlock the token', async function () {
+      const { membershipSBT } = await loadFixture(deployMembershipSBTFixture);
+      await membershipSBT.lock(0);
+      await membershipSBT.unlock(0);
+      expect(await membershipSBT.locked(0)).to.equal(false);
+    });
+
+    it('Should emit an Unlocked event', async function () {
+      const { membershipSBT } = await loadFixture(deployMembershipSBTFixture);
+      await membershipSBT.lock(0);
+      await expect(membershipSBT.unlock(0))
+        .to.emit(membershipSBT, 'Unlocked')
+        .withArgs(0);
+    });
+
+    it('Should fail if not the owner', async function () {
+      const { membershipSBT, addr1 } = await loadFixture(
+        deployMembershipSBTFixture
+      );
+      await expect(membershipSBT.connect(addr1).lock(0)).to.be.reverted;
+    });
+
+    it('Should not allow token transfers', async function () {
+      const { membershipSBT, addr1, addr2 } = await loadFixture(
+        deployMembershipSBTFixture
+      );
+      await membershipSBT.safeMint(addr1.address);
+      await expect(
+        membershipSBT.transferFrom(addr1.address, addr2.address, 0)
+      ).to.be.revertedWith('Token is locked');
     });
   });
 
@@ -65,9 +134,7 @@ describe('MembershipSBT', function () {
         deployMembershipSBTFixture
       );
       await membershipSBT.safeMint(addr1.address);
-      expect(await membershipSBT.tokenURI(0)).to.equal(
-        'https://ipfs.io/ipfs/QmVLqt2tN4NCeYdCsTg1rMeZTmT2D8spowhSnNSLuXtsM2/0'
-      );
+      expect(await membershipSBT.tokenURI(0)).to.equal(`${baseURI}0`);
     });
 
     it('Should fail if not the owner', async function () {
@@ -106,13 +173,21 @@ describe('MembershipSBT', function () {
       );
     });
 
-    it('Should emit an Issued event', async function () {
-      const { membershipSBT, owner, addr1 } = await loadFixture(
+    it('Should increase the tokenId', async function () {
+      const { membershipSBT, addr1, addr2 } = await loadFixture(
         deployMembershipSBTFixture
       );
-      await expect(membershipSBT.safeMint(addr1.address))
-        .to.emit(membershipSBT, 'Issued')
-        .withArgs(owner.address, addr1.address, 0, 0);
+      await membershipSBT.safeMint(addr1.address);
+      await membershipSBT.safeMint(addr2.address);
+      expect(await membershipSBT.tokenURI(1)).to.equal(`${baseURI}1`);
+    });
+
+    it('Should set the right burnAuth', async function () {
+      const { membershipSBT, addr1 } = await loadFixture(
+        deployMembershipSBTFixture
+      );
+      await membershipSBT.safeMint(addr1.address);
+      expect(await membershipSBT.burnAuth(0)).to.equal(0);
     });
 
     it('Should lock the token', async function () {
@@ -123,24 +198,32 @@ describe('MembershipSBT', function () {
       expect(await membershipSBT.locked(0)).to.equal(true);
     });
 
-    it('Should set the right burnAuth', async function () {
-      const { membershipSBT, addr1 } = await loadFixture(
+    it('Should emit an Issued event', async function () {
+      const { membershipSBT, owner, addr1 } = await loadFixture(
         deployMembershipSBTFixture
       );
-      await membershipSBT.safeMint(addr1.address);
-      expect(await membershipSBT.burnAuth(0)).to.equal(0);
+      await expect(membershipSBT.safeMint(addr1.address))
+        .to.emit(membershipSBT, 'Issued')
+        .withArgs(owner.address, addr1.address, 0, 0);
     });
   });
 
-  describe('TransferFrom', function () {
-    it('Should not allow token transfers', async function () {
-      const { membershipSBT, addr1, addr2 } = await loadFixture(
+  describe('Minting Batch', function () {
+    it('Should mint multiple tokens', async function () {
+      const { membershipSBT, owner, addr1 } = await loadFixture(
         deployMembershipSBTFixture
       );
-      await membershipSBT.safeMint(addr1.address);
-      await expect(
-        membershipSBT.transferFrom(addr1.address, addr2.address, 0)
-      ).to.be.revertedWith('Token is locked');
+      await membershipSBT.safeMintBatch([owner.address, addr1.address]);
+      expect(await membershipSBT.balanceOf(owner.address)).to.equal(1);
+      expect(await membershipSBT.balanceOf(addr1.address)).to.equal(1);
+    });
+
+    it('Should fail if not the owner', async function () {
+      const { membershipSBT, addr1 } = await loadFixture(
+        deployMembershipSBTFixture
+      );
+      await expect(membershipSBT.connect(addr1).safeMintBatch([addr1.address]))
+        .to.be.reverted;
     });
   });
 
