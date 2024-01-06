8b886ea2940764be406edc86cc3ffc3933abd892
kei-matsuoka <keimatsuoka0311@gmail.com>
2024-01-06 15:20:28 +0900

    scriptの修正

--- scripts/createMembershipSBT.ts
+++ scripts/createMembershipSBT.ts
@@ -1,6 +1,21 @@
 import { ethers } from 'hardhat';
 
 async function main() {
+  const name = 'Fitness Gym Membership';
+  const symbol = 'FGM';
+  const baseURI =
+    'https://ipfs.io/ipfs/QmVLqt2tN4NCeYdCsTg1rMeZTmT2D8spowhSnNSLuXtsM2/';
+  const maxSupply = 2;
+  const burnAuth = { IssuerOnly: 0, OwnerOnly: 1, Both: 2, Neither: 3 };
+
+  const metadata = {
+    name: 'Fitness Gym Membership',
+    image:
+      'https://ipfs.io/ipfs/QmWU8qrJm4ByGdSSyoaqvG4YbzAm4EZFHQG7L7LLCVfwvM',
+    description:
+      'This is a membership token for the Fitness Gym. It is a soul-bound token (SBT) that can be used to redeem a 1 year membership at the Fitness Gym.',
+  };
+
   // SBTFactoryを取得
   const sbtFactory = await ethers.getContractAt(
     'SBTFactory',
@@ -9,13 +24,13 @@ async function main() {
 
   // createMembershipSBT
   let tx = await sbtFactory.createMembershipSBT(
-    'Fitness Gym Membership',
-    'FGM',
-    'https://ipfs.io/ipfs/QmVLqt2tN4NCeYdCsTg1rMeZTmT2D8spowhSnNSLuXtsM2/',
-    100,
-    0,
-    'https://ipfs.io/ipfs/QmWU8qrJm4ByGdSSyoaqvG4YbzAm4EZFHQG7L7LLCVfwvM',
-    'This is a membership token for the Fitness Gym. It is a soul-bound token (SBT) that can be used to redeem a 1 year membership at the Fitness Gym.',
+    name,
+    symbol,
+    baseURI,
+    maxSupply,
+    burnAuth.IssuerOnly,
+    metadata.image,
+    metadata.description,
     { value: ethers.parseEther('0.01') }
   );
 
